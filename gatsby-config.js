cc3d74c22adc8ff271b812dbc415a93635586d93
Jessamine Brown <brownjessa11@gmail.com>
2021-04-29 14:42:16 -0500

    comma

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-gatsby-cloud'
+    'gatsby-plugin-gatsby-cloud',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
